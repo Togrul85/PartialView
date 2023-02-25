3c44cc6dc882059897a4770247b92a6ca08787ef
Togrul85 <toghrulkhs@code.edu.az>
2023-02-25 19:51:06 +0400

    .

---
+++ FirelloProject/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using FirelloProject.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirelloProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ProductController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.ProductCount = _appDbContext.Products.Count();
+            var products = _appDbContext.Products.Include(p => p.ProductImages).Include(p=>p.Category)
+                .Take(2).ToList();
+            return View(products);
+        }
+        public IActionResult LoadMore(int skip)
+        {
+            var products=_appDbContext.Products.Include(p=>p.ProductImages)
+                .Include(p=>p.Category)
+                .Skip(skip)
+                .Take(2)
+                .ToList();
+            return PartialView("_ProductLoadMorePartial",products);
+        }
+    }
+}
